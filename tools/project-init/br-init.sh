b07e5b00de8ec000a7023ae90b5f60ce5dd20f5b
Alexa Amundson <118287761+blackboxprogramming@users.noreply.github.com>
2026-02-22 20:14:53 -0600

    feat: br-init.sh brand palette sweep

--- tools/project-init/br-init.sh
+++ tools/project-init/br-init.sh
@@ -1,5 +1,8 @@
 #!/bin/zsh
 # BR Init - Project Scaffolder
+AMBER='\033[38;5;214m'; PINK='\033[38;5;205m'; VIOLET='\033[38;5;135m'; BBLUE='\033[38;5;69m'
+GREEN='\033[0;32m'; RED='\033[0;31m'; BOLD='\033[1m'; DIM='\033[2m'; NC='\033[0m'
+CYAN="$AMBER"; YELLOW="$PINK"; BLUE="$BBLUE"; MAGENTA="$VIOLET"; PURPLE="$VIOLET"
 INIT_HOME="/Users/alexa/blackroad/tools/project-init"
 GREEN='\033[0;32m'; CYAN='\033[0;36m'; YELLOW='\033[1;33m'; BLUE='\033[0;34m'; NC='\033[0m'
 
